5abd70887746bd84863f10e9a45940c59273f49c
SUN Hao <sunhaohit0@gmail.com>
2016-03-01 13:10:48 +0800

    construct basic structure of vlblocks-python

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vlblocks-python',
+    version='0.0.1',
+    author='SUN Hao',
+    author_email='sunhaohit0@gmial.com',
+    packages=find_packages(),
+    url='https://github.com/hmilhy/vlblocks-python',
+    zip_safe=False,
+)
+
